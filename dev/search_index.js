7b6f6651359fcc6a8f80c97c30eee75f6517140e
Documenter.jl <documenter@juliadocs.github.io>
2025-01-26 22:13:07 +0000

    build based on ef9cd9c

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = TestPicker","category":"page"},{"location":"#TestPicker","page":"Home","title":"TestPicker","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for TestPicker.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [TestPicker]","category":"page"}]
+}
