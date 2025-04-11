b5fd16d8cc1af7a8587bb372e732ca0a260105ee
Théo Galy-Fajou <theo.galyfajou@gmail.com>
2025-04-11 10:13:42 +0200

    fix test

--- test/eval.jl
+++ test/eval.jl
@@ -1,12 +1,16 @@
 using Test
 using Pkg: PackageSpec
-using TestPicker: TestPicker, eval_in_module, current_pkg
+using TestPicker: TestPicker, eval_in_module, current_pkg, TestInfo
 
 @testset "Test eval in module" begin
     path = pkgdir(TestPicker)
     pkg_spec = PackageSpec(; name="TestPicker", path)
-    @test isnothing(eval_in_module(:(sin(3)), pkg_spec))
+    @test isnothing(eval_in_module(TestInfo(:(sin(3)), "eval.jl", "", 0), pkg_spec))
     # Errors don't disturb the env
-    @test_throws ErrorException eval_in_module(:(error("🤯")), pkg_spec)
-    @test isnothing(eval_in_module(:(@testset "foo" begin end), pkg_spec))
+    @test_throws ErrorException eval_in_module(
+        TestInfo(:(error("🤯")), "eval.jl", "error", 2), pkg_spec
+    )
+    @test isnothing(
+        eval_in_module(TestInfo(:(@testset "foo" begin end), "eval.jl", "foo", 10), pkg_spec)
+    )
 end
