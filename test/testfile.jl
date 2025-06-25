eb6965eaa2872d39be6863dae11ee3f5a3113c7f
Alberto Mengali <disberd@gmail.com>
2025-06-25 17:01:13 +0200

    add test

--- test/testfile.jl
+++ test/testfile.jl
@@ -13,6 +13,7 @@ using TestPicker: EvalTest, get_test_files, run_test_file
         [
             "sandbox/test-a.jl",
             "sandbox/test-b.jl",
+            "sandbox/test-testitem.jl",
             "sandbox/test-subdir/test-file-c.jl",
             "sandbox/weird-name.jl",
         ],
@@ -29,4 +30,11 @@ end
     @test TestPicker.LATEST_EVAL[] isa Vector{EvalTest}
     evaltest = only(TestPicker.LATEST_EVAL[])
     @test evaltest.info.filename == "sandbox/test-a.jl"
+end
+
+@testset "Running a file containing @testitem tests" begin
+    path = pkgdir(TestPicker)
+    pkg_spec = PackageSpec(; name="TestPicker", path)
+    # We test that this correctly executes without errors mostly here
+    @test run_test_file(joinpath(@__DIR__, "sandbox/test-testitem.jl"), pkg_spec) === nothing
 end
